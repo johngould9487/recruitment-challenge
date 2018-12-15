4a9c4988cf7aa75ea134afb75ab2418df641a0e8
John Gould <johngould9487@gmail.com>
2018-12-15 10:06:25 +0000

    updated game display

--- DotNet/DotNet/GameDisplay.cs
+++ DotNet/DotNet/GameDisplay.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DotNet
 {
@@ -11,6 +12,39 @@ namespace DotNet
         {
             _deck = deck;
             _players = players;
+        }
+
+        public void Update()
+        {
+            Console.Clear();
+            int index = 1;
+            foreach (var card in _deck.Cards)
+            {
+                if (card._turnedUp) Console.Write("[X]");
+                else Console.Write("[ ]");
+                if (index % 13 == 0) Console.WriteLine();
+                index++;
+            }
+            foreach (var player in _players)
+            {
+                Console.WriteLine($"{player.ToString()} has {player.NumberOfTricks} tricks");
+            }
+        }
+
+        public void PromptUser()
+        {
+            Console.WriteLine("Press 'Enter' to pick a card");
+            Console.ReadKey(true);
+        }
+
+        public void DisplayTrick(Player player)
+        {
+            Console.WriteLine($"{player.ToString()} matched the {player.currentCards[0].ToString()} with the {player.currentCards[1].ToString()}");
+        }
+
+        public void ShowChosenCard(Card card, Player player)
+        {
+            Console.WriteLine($"{player.ToString()} chose the {card.ToString()}");
         }
     }
 }
